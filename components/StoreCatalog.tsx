627d61b94f353f36c01336fe9b92a4d4ed6b55ad
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-07-02 18:12:43 -0300

    refactor(components): refactoring code to use the ProductCard component

--- components/StoreCatalog.tsx
+++ components/StoreCatalog.tsx
@@ -1,12 +1,18 @@
-import { Container } from "../styles/StoreCatalog";
 
-export default function StoreCatalog() {
+import { Container } from "../styles/StoreCatalog";
+import ProductCards, { PropsProductCards } from "./ProductCards";
+
+export default function StoreCatalog({ data }: { data: PropsProductCards[] }) {
   return (
     <Container>
-        <p>49 produtos encontrados</p>
-        <div>
-          Catalog
-        </div>
+      <p>{data.length} produtos encontrados</p>
+      <div>
+        {
+          data.map(
+            (product, i) => <ProductCards {...product} key={i} />
+          )
+        }
+      </div>
     </Container>
   )
 }
