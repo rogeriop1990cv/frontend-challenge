8944869c9ec1115d29565ef19a9f6a9a43814f71
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-07-02 18:06:18 -0300

    feat(pages/index): was add function getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { type } from 'os'
 import NavBar from '../components/NavBar'
+import { PropsProductCards } from '../components/ProductCards'
 import RefinePrice from '../components/RefinePrice'
 import StoreCatalog from '../components/StoreCatalog'
 import { Content } from '../styles/Index'
 
+const BASE_URL = 'https://wine-back-test.herokuapp.com/products?page=1&limit=10'
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const res = await fetch(BASE_URL)
+  const data = await res.json()
+  return {
+    props: {
+      productsList: data.items,
+    },
+  }
+}
+
+type PropsHome = {
+  productsList: PropsProductCards[]
+}
+
+const Home: NextPage<PropsHome> = ({ productsList }) => {
   return (
     <div>
       <Head>
@@ -15,7 +32,7 @@ const Home: NextPage = () => {
       <NavBar />
       <Content>
         <RefinePrice />
-        <StoreCatalog />
+        <StoreCatalog data={ productsList } />
       </Content>
     </div>
   )
