ead0136a0e1ed7c995aa25d1863a8fb67c3d7ddf
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-07-02 22:04:30 -0300

    style: created the css of the ExceptionalValue component

---
+++ styles/ExceptionalValue.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const FormatValue = styled.div`
+  
+  > p {
+    margin: 0;
+    margin-left: 5px;
+    color: #B6116E;
+  }
+  > p > span {
+    font-size: 23px;
+  }
+`
