9585beab331741add6ae58cce404a2f4a163ac29
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-07-03 03:54:57 -0300

    feat(components/StoreCatalog): useState has been created for the component

--- components/StoreCatalog.tsx
+++ components/StoreCatalog.tsx
@@ -1,18 +1,34 @@
 
-import { Container } from "../styles/StoreCatalog";
+import { useState } from "react";
+import { paginationGeneratorIndex } from "../helper";
+import { Catalog, Container } from "../styles/StoreCatalog";
+import PageNavigation from "./PageNavigation";
 import ProductCards, { PropsProductCards } from "./ProductCards";
 
+
 export default function StoreCatalog({ data }: { data: PropsProductCards[] }) {
+
+  const [productList, _setProductList] = useState(data)
+  const [page, setPage] = useState(1)
+
+
+  const pagingIndex: {
+    [key: number]: number[]
+  } = paginationGeneratorIndex(data.length)
+
   return (
     <Container>
-      <p>{data.length} produtos encontrados</p>
       <div>
+        <p>{data.length} produtos encontrados</p>
+      </div>
+      <Catalog>
         {
-          data.map(
+          productList.slice(pagingIndex[page][0], pagingIndex[page][1]).map(
             (product, i) => <ProductCards {...product} key={i} />
           )
         }
-      </div>
+      </Catalog>
+      <PageNavigation setPage={setPage} />
     </Container>
   )
 }
